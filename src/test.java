1e2e13d1ef2e47a91e491541845361544324defe
carslan3455 <carslan3455@gmail.com>
2020-11-24 20:36:20 +0100

    add test

---
+++ src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("1.Islem");
+
+
+    }
+    public static void yeniMethod(){
+
+        System.out.println("Yeni method hazir");
+
+        System.out.println("Sepete ekleme tamamlandi");
+    }
+}
